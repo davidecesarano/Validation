1698799417ff35bed1b1a63d4471583ca508b0d9
Davide Cesarano <davide.cesarano@unipegaso.it>
2017-03-28 11:17:33 +0200

    Modifica patterns e metodi
    Aggiunti: verifica array, metodo purify, revisione di alcuni patterns,
    risolto bug metodo ext.

--- Validation.php
+++ Validation.php
@@ -1,5 +1,5 @@
 <?php
-	
+    
     /**
      * Validation 
      *
@@ -10,35 +10,35 @@
      * @license https://github.com/davidecesarano/Validation/blob/master/LICENSE MIT License
      * @link https://github.com/davidecesarano/Validation
      */
-	 
+     
     class Validation {
-		
+        
         /**
          * @var array $patterns
          */
         public $patterns = array(
-            'uri'       => '[A-Za-z0-9-\/_]+',
-            'url'       => '[A-Za-z0-9-:.\/_]+',
-            'alpha'     => '[A-Za-z]+',
-            'words'     => '[\p{L}\s]+',
-            'alphanum'  => '[A-Za-z0-9]+',
-            'int'       => '[0-9]+',
-            'float'     => '[[0-9\.,]+',
-            'tel'       => '[0-9+\s()-]+',
-            'text'      => '[\p{L}0-9\s-.,;:!"%&()?\'°#]+',
-            'file'      => '[A-Za-z0-9-_]+\.[A-Za-z0-9]{2,4}',
-            'folder'    => '[A-Za-z0-9-_]+',
-            'address'   => '[\p{L}0-9\s.,()°-]+',
-            'date_dmy'  => '[0-9]{1,2}\-[0-9]{1,2}\-[0-9]{4}',
-            'date_ymd'  => '[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}',
-            'email'     => '[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+'
+            'uri'           => '[A-Za-z0-9-\/_?&=]+',
+            'url'           => '[A-Za-z0-9-:.\/_?&=#]+',
+            'alpha'         => '[\p{L}]+',
+            'words'         => '[\p{L}\s]+',
+            'alphanum'      => '[\p{L}0-9]+',
+            'int'           => '[0-9]+',
+            'float'         => '[0-9\.,]+',
+            'tel'           => '[0-9+\s()-]+',
+            'text'          => '[\p{L}0-9\s-.,;:!"%&()?+\'°#\/@]+',
+            'file'          => '[\p{L}\s0-9-_!%&()=\[\]#@,.;+]+\.[A-Za-z0-9]{2,4}',
+            'folder'        => '[\p{L}\s0-9-_!%&()=\[\]#@,.;+]+',
+            'address'       => '[\p{L}0-9\s.,()°-]+',
+            'date_dmy'      => '[0-9]{1,2}\-[0-9]{1,2}\-[0-9]{4}',
+            'date_ymd'      => '[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}',
+            'email'         => '[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+'
         );
-
+        
         /**
          * @var array $errors
          */
         public $errors = array();
-
+        
         /**
          * Nome del campo
          * 
@@ -49,9 +49,9 @@
             
             $this->name = $name;
             return $this;
-
+        
         }
-
+        
         /**
          * Valore del campo
          * 
@@ -62,22 +62,22 @@
             
             $this->value = $value;
             return $this;
-
+        
         }
         
         /**
-		 * File
-		 * 
-		 * @param mixed $value
-		 * @return this
-		 */
-		public function file($value){
-			
-			$this->file = $value;
-			return $this;
-		
-		}
-
+         * File
+         * 
+         * @param mixed $value
+         * @return this
+         */
+        public function file($value){
+            
+            $this->file = $value;
+            return $this;
+        
+        }
+        
         /**
          * Pattern da applicare al riconoscimento
          * dell'espressione regolare
@@ -87,14 +87,24 @@
          */
         public function pattern($name){
             
-            $regex = '/^('.$this->patterns[$name].')$/u';
-            if($this->value != '' && !preg_match($regex, $this->value)){
-                $this->errors[] = 'Formato campo '.$this->name.' non valido.';
+            if($name == 'array'){
+                
+                if(!is_array($this->value)){
+                    $this->errors[] = 'Formato campo '.$this->name.' non valido.';
+                }
+            
+            }else{
+            
+                $regex = '/^('.$this->patterns[$name].')$/u';
+                if($this->value != '' && !preg_match($regex, $this->value)){
+                    $this->errors[] = 'Formato campo '.$this->name.' non valido.';
+                }
+                
             }
             return $this;
             
         }
-
+        
         /**
          * Pattern personalizzata
          * 
@@ -110,21 +120,21 @@
             return $this;
             
         }
-
+        
         /**
-		 * Campo obbligatorio
-		 * 
-		 * @return this
-		 */
-		public function required(){
-			
-			if((isset($this->file) && $this->file['error'] == 4) || ($this->value == '' || $this->value == null)){
-				$this->errors[] = 'Campo '.$this->name.' obbligatorio.';
-			}            
-			return $this;
-			
-		}
-
+         * Campo obbligatorio
+         * 
+         * @return this
+         */
+        public function required(){
+            
+            if((isset($this->file) && $this->file['error'] == 4) || ($this->value == '' || $this->value == null)){
+                $this->errors[] = 'Campo '.$this->name.' obbligatorio.';
+            }            
+            return $this;
+            
+        }
+        
         /**
          * Lunghezza minima
          * del valore del campo
@@ -133,13 +143,13 @@
          * @return this
          */
         public function min($length){
-
+            
             if(is_string($this->value)){
                 
                 if(strlen($this->value) < $length){
                     $this->errors[] = 'Valore campo '.$this->name.' inferiore al valore minimo';
                 }
-
+           
             }else{
                 
                 if($this->value < $length){
@@ -148,9 +158,9 @@
                 
             }
             return $this;
-
+            
         }
-			
+            
         /**
          * Lunghezza massima
          * del valore del campo
@@ -159,13 +169,13 @@
          * @return this
          */
         public function max($length){
-
+            
             if(is_string($this->value)){
                 
                 if(strlen($this->value) > $length){
                     $this->errors[] = 'Valore campo '.$this->name.' superiore al valore massimo';
                 }
-
+           
             }else{
                 
                 if($this->value > $length){
@@ -174,9 +184,9 @@
                 
             }
             return $this;
-
+            
         }
-
+        
         /**
          * Confronta con il valore di
          * un altro campo
@@ -185,7 +195,7 @@
          * @return this
          */
         public function equal($value){
-
+        
             if($this->value != $value){
                 $this->errors[] = 'Valore campo '.$this->name.' non corrispondente.';
             }
@@ -216,13 +226,23 @@
          */
         public function ext($extension){
 
-            if($this->file['error'] != 4 && pathinfo($this->file['name'], PATHINFO_EXTENSION) != $extension){
+            if($this->file['error'] != 4 && pathinfo($this->file['name'], PATHINFO_EXTENSION) != $extension && strtoupper(pathinfo($this->file['name'], PATHINFO_EXTENSION)) != $extension){
                 $this->errors[] = 'Il file '.$this->name.' non è un '.$extension.'.';
             }
             return $this;
             
         }
-
+        
+        /**
+         * Purifica per prevenire attacchi XSS
+         *
+         * @param string $string
+         * @return $string
+         */
+        public function purify($string){
+            return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+        }
+        
         /**
          * Campi validati
          * 
@@ -231,7 +251,7 @@
         public function isSuccess(){
             if(empty($this->errors)) return true;
         }
-
+        
         /**
          * Errori della validazione
          * 
@@ -240,7 +260,7 @@
         public function getErrors(){
             if(!$this->isSuccess()) return $this->errors;
         }
-
+        
         /**
          * Visualizza errori in formato Html
          * 
@@ -277,7 +297,7 @@
             }
         
         }
-
+        
         /**
          * Verifica se il valore è
          * un numero intero
@@ -288,7 +308,7 @@
         public static function is_int($value){
             if(filter_var($value, FILTER_VALIDATE_INT)) return true;
         }
-
+        
         /**
          * Verifica se il valore è
          * un numero float
@@ -299,7 +319,7 @@
         public static function is_float($value){
             if(filter_var($value, FILTER_VALIDATE_FLOAT)) return true;
         }
-
+        
         /**
          * Verifica se il valore è
          * una lettera dell'alfabeto
@@ -310,7 +330,7 @@
         public static function is_alpha($value){
             if(filter_var($value, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => "/^[a-zA-Z]+$/")))) return true;
         }
-
+        
         /**
          * Verifica se il valore è
          * una lettera o un numero
@@ -321,29 +341,29 @@
         public static function is_alphanum($value){
             if(filter_var($value, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => "/^[a-zA-Z0-9]+$/")))) return true;
         }
-
-        /**
-		 * Verifica se il valore è
-		 * un url
-		 *
-		 * @param mixed $value
-		 * @return boolean
-		 */
-		public static function is_url($value){
-			if(filter_var($value, FILTER_VALIDATE_URL)) return true;
-		}
         
         /**
-		 * Verifica se il valore è
-		 * un uri
-		 *
-		 * @param mixed $value
-		 * @return boolean
-		 */
-		public static function is_uri($value){
-			if(filter_var($value, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => "/^[A-Za-z0-9-\/_]+$/")))) return true;
-		}
-
+         * Verifica se il valore è
+         * un url
+         *
+         * @param mixed $value
+         * @return boolean
+         */
+        public static function is_url($value){
+            if(filter_var($value, FILTER_VALIDATE_URL)) return true;
+        }
+        
+        /**
+         * Verifica se il valore è
+         * un uri
+         *
+         * @param mixed $value
+         * @return boolean
+         */
+        public static function is_uri($value){
+            if(filter_var($value, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => "/^[A-Za-z0-9-\/_]+$/")))) return true;
+        }
+        
         /**
          * Verifica se il valore è
          * true o false
@@ -354,7 +374,7 @@
         public static function is_bool($value){
             if(filter_var($value, FILTER_VALIDATE_BOOLEAN)) return true;
         }
-
+        
         /**
          * Verifica se il valore è
          * un'e-mail
@@ -365,5 +385,5 @@
         public static function is_email($value){
             if(filter_var($value, FILTER_VALIDATE_EMAIL)) return true;
         }
-
-    }    
+        
+    }
